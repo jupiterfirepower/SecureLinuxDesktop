1e8eaece6b49a02877a308beb6c1bb3223791a71
Oleh Parshenko <jupiterfirepower@gmail.com>
2018-04-02 12:00:44 +0300

    original script

--- hdesktop.sh
+++ hdesktop.sh
@@ -46,49 +46,26 @@ f_mylogindefs	# 60_logindefs
 f_securedelete	# 62_securedelete
 f_pampassword	# 63_pampassword
 f_firefoxuserpref # 67_firefoxuserpref
-#f_disablenet	# 03_disablenet
-#f_disablemnt	# 04_disablemnt
-#f_disablemod   # 23_disablemod
-#f_systemdconf	# 05_systemdconf
-#f_resolvedconf # 32_resolvedconf
-#f_logindconf	# 31_logindconf
-f_journalctl    # 06_journalctl - 
 f_timesyncd		# 07_timesyncd
 f_coredump		# 37_coredump
-#f_fstab		# 08_fstab -
-#f_prelink		# 09_prelink not for desktop Prelink и Preload для ускорения запуска программ в Linux
 f_aptget		# 10_aptget
 f_hosts		    # 11_hosts
 f_issue		    # 34_issue
-#f_logindefs	# 12_logindefs -
-#f_sysctl		# 13_sysctl - NOT FOR UBUNTU
-#f_limitsconf	# 14_limits
-#f_adduser		# 15_adduser -
-#f_rootaccess	# 16_rootaccess securetty -
 f_packages		# 17_packages
 f_cronrkhunter	# 65_cronrkhunter
-#f_apport		# 35_apport
-#f_rkhunter		# 33_rkhunter
-#f_sshdconfig	# 18_sshdconfig
-#f_password		# 19_password
-#f_cron			# 20_cron  - not for desktop
 f_ctrlaltdel	# 21_ctraltdel
 f_auditd		# 22_auditd
 f_aide			# 24_aide
 f_rhosts		# 25_rhosts
-#f_users		# 26_users
 f_lockroot		# 36_lockroot
 f_aptget_clean	# 10_aptget
 f_hardfirefox	# 70_HardeninFirefox
-#f_suid			# 27_suid
-#f_umask		# 28_umask
-#f_path			# 30_path
 f_aa_enforce	# 29_apparmor
 f_aide_post	# 24_aide
 f_aide_timer	# 24_aide
 f_systemddelta	# 98_systemddelta
 f_checkreboot	# 99_reboot
-#f_samhain	# 73_samhain
+f_samhain	# 73_samhain
 f_snort		# 74_snort
 f_myfstab	# 61_fstab
 echo
